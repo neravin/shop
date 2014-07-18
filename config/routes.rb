bd54324f5b428fe4d0855d7b459581b36caae163
Flash <admin@MacBook-Pro-15---admin.local>
2014-07-18 23:17:49 +0400

    Создание ProductCategory

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :categories
+
   resources :vids
 
   resources :bodies
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607143919) do
+ActiveRecord::Schema.define(version: 20140718105155) do
 
   create_table "bodies", force: true do |t|
     t.string   "pol"
@@ -28,6 +28,20 @@ ActiveRecord::Schema.define(version: 20140607143919) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "categories", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories_products", id: false, force: true do |t|
+    t.integer "category_id", null: false
+    t.integer "product_id",  null: false
+  end
+
+  add_index "categories_products", ["category_id"], name: "index_categories_products_on_category_id"
+  add_index "categories_products", ["product_id"], name: "index_categories_products_on_product_id"
 
   create_table "line_items", force: true do |t|
     t.integer  "product_id"
