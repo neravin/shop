98a6d50c4c702826a6a45c231770181f9b03333d
Neravin <neravind@gmail.com>
2014-07-20 10:32:51 +0400

    Finish simple sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,11 +7,22 @@ module SessionsHelper
     self.current_client = client
   end
 
+  def signed_in?
+    !current_client.nil?
+  end
+
   def current_client=(client)
     @current_client = client
   end
   def current_client
     remember_token = Client.encrypt(cookies[:remember_token])
     @current_client ||= Client.find_by(remember_token: remember_token)
+  end
+
+  def sign_out
+    current_client.update_attribute(:remember_token,
+                                  Client.encrypt(Client.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_client = nil
   end
 end
