f4868ad8cff255e63123e71be842685fe568f9c7
Neravin <neravind@gmail.com>
2014-08-11 10:51:33 +0400

    add last view products

--- app/assets/javascripts/last_view.js
+++ app/assets/javascripts/last_view.js
@@ -1,15 +1,128 @@
-window.onload = init;
+$(document).on('page:change', function () {
+  var products = document.getElementsByClassName("link-product");
+  for (var i = 0; i < products.length; i++) {
+    products[i].onclick = addViewed;
+  }
+  addLastProductsToDom();
+});
+
+/*window.onload = init;
 
 function init() {
-  var products = document.getElementsByTagName("li");
-  for (var i = 0; i < products.length; i++) {
-    console.log('testq');
-    products[i].oncklick = addViewed;
+  addLastProductsToDom();
+}*/
+
+function Product(id, title, price, imageSrc) {
+  this.id        = id;
+  this.title     = title;
+  this.price     = price;
+  this.imageSrc = imageSrc;
+}
+
+function addLastProductsToDom() {
+  var ul   = document.getElementById("last-view");
+
+  if(ul && (ul.childNodes.length <= 1)){
+    products = getProductsFromStorage();
+    for ( var i = 0; i < products.length; i++ ){
+      var li   = document.createElement("li");
+      var a    = document.createElement("a");
+      var href = "/products/" + products[i].id;
+      var h3   = document.createElement("h3");
+
+      a.setAttribute("class", "link-product");
+      a.setAttribute("href", href);
+      h3.setAttribute("class", "product-title");
+      h3.innerHTML = products[i].title;
+
+      a.appendChild(h3);
+      li.appendChild(a);
+      ul.appendChild(li);
+    }
   }
 }
 
 function addViewed(e) {
-  alert("Valero");
-  var id = e.target.parentNode.id;
-  alert(id);
+  var product = getProductDOM(e);
+  createProductToLast(product);
+}
+
+function getProductDOM(e) {
+  var link = e.target;
+  var id_link;
+  var id;
+  var title;
+  var priceWithRub;
+  var price;
+  var imageSrc;
+
+  while (link.className.toLowerCase() != "link-product"){
+    link = link.parentNode;
+  }
+
+  id_link = link.getAttribute("id").split('-');
+  id = parseInt(id_link[1], 10);
+  title = link.getElementsByClassName('product-title')[0].innerText;
+  priceWithRub = link.getElementsByClassName('price')[0].innerText;
+  // removal of superfluous commas from the price
+  price = parseInt(priceWithRub.slice(1).replace(/\,/g, ""));
+  imageSrc = link.getElementsByTagName('img')[0].src;
+
+  product = new Product(id, title, price, imageSrc);
+  return product;
+}
+
+function createProductToLast(productObj) {
+  var productsArray = getProductsArray();
+  var currentData   = new Date();
+  var key           = "product_" + currentData.getTime();
+  var maxElemets    = 10;
+
+  if(!isRepeatedOrMany(productObj, maxElemets)){
+    localStorage.setItem(key, JSON.stringify(productObj));
+    productsArray.push(key);
+    localStorage.setItem("productsArray", JSON.stringify(productsArray));
+  }
+}
+
+function getProductsArray() {
+  var productsArray = localStorage.getItem("productsArray");
+
+  if( !productsArray ){
+    productsArray = [];
+    localStorage.setItem("productsArray", JSON.stringify(productsArray));
+  }
+  else{
+    productsArray = JSON.parse(productsArray);
+  }
+
+  return productsArray;
+}
+
+function getProductsFromStorage() {
+  var products = [];
+  var productsArray = getProductsArray();
+
+  for( var i = 0; i < productsArray.length; i++ ) {
+    products.push( JSON.parse(localStorage.getItem(productsArray[i])) );
+  }
+  return products; 
+}
+
+function isRepeatedOrMany(productObj, maxElemets) {
+  var isRepeated    = false;
+  var productsArray = getProductsArray();
+
+  if (productsArray.length < maxElemets){
+    for( var i = 0; i < productsArray.length; i++ ) {
+      if(productObj['id'] == JSON.parse(localStorage.getItem(productsArray[i]))['id'] ){
+        isRepeated = true;
+        break;
+      }
+    }
+  }
+  else{
+    isRepeated = true;
+  }
+  return isRepeated;
 }
